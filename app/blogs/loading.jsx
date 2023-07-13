1655c8c20dde3b7658f330d18f95f54ec9d74072
technicalranjitofficial <dranjitkumar16@gmail.com>
2023-07-13 07:40:37 +0530

    Fixed routing

--- app/blogs/loading.jsx
+++ app/blogs/loading.jsx
@@ -4,7 +4,7 @@ export default function Loading(){
     return  <div className="min-h-screen w-full z-0 flex justify-center items-center">
      <div className='w-full h-full fixed bg-transparent text-white z-40 flex items-center justify-center'>
 
-        {/* <h1>Loading..</h1> */}
+        <h1>Loading..</h1>
 {/* <Loader/> */}
 
 </div>
