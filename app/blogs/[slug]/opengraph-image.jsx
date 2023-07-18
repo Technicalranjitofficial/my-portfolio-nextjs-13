f1b41536358b89d5c76353e3533208ea1d21d8e4
technicalranjitofficial <dranjitkumar16@gmail.com>
2023-07-18 16:34:02 +0530

    Seo Added

--- app/blogs/[slug]/opengraph-image.jsx
+++ app/blogs/[slug]/opengraph-image.jsx
@@ -18,12 +18,12 @@ export default async function og({ params }) {
     (
       <div tw="relative flex items-center justify-center">
         <img src={data.allBlog[0].poster.asset.url} alt={data.allBlog[0].title} />
-        <div tw="absolute flex bg-black opacity-50 inset-0 " />
+        {/* <div tw="absolute flex bg-black opacity-50 inset-0 " />
         <div tw="absolute flex items-center top-2 w-full ">
           <p tw="text-white text-4xl flex font-bold m-5">{data.allBlog[0].title}</p>
           <p tw="text-indigo-200 text-xl flex font-bold m-5">Ranjit Das</p>
           <p tw="text-purple-200 text-xl flex font-bold m-5">{data.allBlog[0].createdAt}</p>
-        </div>
+        </div> */}
       </div>
     ),
     size
